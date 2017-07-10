c8e6da45ae17c2de9de5cda74e1044cab76dc200
mikhailman <mishanya.v91@mail.ru>
2017-07-10 18:10:15 +0300

    first version

---
+++ mansbot_bot.rb
@@ -0,0 +1,25 @@
+require 'telegram/bot'
+require 'dotenv'
+
+Dotenv.load('.env')
+token = ENV['TELEGRAM_BOT_API_KEY']
+
+
+def telegram_send(bot, message, s_message)
+  bot.api.sendMessage(chat_id: message.chat.id, text: s_message)
+end
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      telegram_send(bot, message, "Привет, #{message.from.first_name}. Сколько раз ты хочешь, что бы жируша отжалась? 10 раз? 20? 30?:)")
+    when '10'
+      telegram_send(bot, message, "Жируша! Быстро отожмись 10 раз!")
+    when '20'
+      telegram_send(bot, message, "Жируша! Спорим, 20 раз ты не осилишь!?")
+    when '30'
+      telegram_send(bot, message, "Ну, 30 раз Жируша точно не смогЁт!)")
+    end
+  end
+end
