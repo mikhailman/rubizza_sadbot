cf482829dd17583e7f8a9264b3c2167f923da4cf
mikhailman <mishanya.v91@mail.ru>
2017-07-11 08:49:56 +0300

    check single quotes

--- mansbot_bot.rb
+++ mansbot_bot.rb
@@ -13,15 +13,15 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      telegram_send(bot, message, "Привет, #{message.from.first_name}. Сколько раз ты хочешь, что бы жируша отжалась? 10 раз? 20? 30?:)")
+      telegram_send(bot, message, 'Привет, #{message.from.first_name}. Сколько раз ты хочешь, что бы жируша отжалась? 10 раз? 20? 30?:)')
     when '10'
-      telegram_send(bot, message, "Жируша! Быстро отожмись 10 раз!")
+      telegram_send(bot, message, 'Жируша! Быстро отожмись 10 раз!')
     when '20'
-      telegram_send(bot, message, "Жируша! Спорим, 20 раз ты не осилишь!?")
+      telegram_send(bot, message, 'Жируша! Спорим, 20 раз ты не осилишь!?')
     when '30'
-      telegram_send(bot, message, "Ну, 30 раз Жируша точно не смогЁт!)")
+      telegram_send(bot, message, 'Ну, 30 раз Жируша точно не смогЁт!)')
     when /^\d+$/
-      telegram_send(bot, message, "Чет не то ты пишешь -_-")
+      telegram_send(bot, message, 'Чет не то ты пишешь -_-')
     end
   end
 end
